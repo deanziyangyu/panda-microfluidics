f6339d65954138f6cc7657674bc90fd236471aae
deanziyangyu <deanziyangyu@gmail.com>
2025-03-26 17:05:17 -0400

    Added test gui for matching fiducials

---
+++ misc_src/match_markers_gui.py
@@ -0,0 +1,469 @@
+# import apriltag
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+""" A OpenCV-based Webcam Server Listening on [IP] [PORT]
+Usage:
+    for startup without path and timestamp specified:
+        py ./webcam_server.py --mode s
+    for startup specifying path and timestamp:
+        py ./webcam_server.py --mode v --path ./path/to/data_collection_folder/YYMMDD_HHMMSS \
+            --init_time YYMMDD_HHMMSS
+
+Shell launch cmd from other .py program:
+    silent: Popen(["py", "./8_utilities/webcam_server.py", "--mode", "s"])
+    verbose: Popen(["py", "./8_utilities/webcam_server.py",
+        "--mode", "v",
+        "--path", "./path/to/data_collection_folder/YYMMDD_HHMMSS",
+        "--init_time", "YYMMDD_HHMMSS"])
+"""
+import os
+import sys
+import time
+import socket
+import logging
+import datetime
+import argparse
+
+import csv
+import cv2
+from PyQt5.QtWidgets import QApplication, QLabel, QHBoxLayout, QVBoxLayout, QWidget, QPushButton
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, QObject, QTimer, QSize
+
+FILE_SET = False
+CAMERA_NUM = 0
+
+HOST = '127.0.0.1'  # Server IP address
+PORT = 12345       # Server port number
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s'
+)
+
+csv_fields = ['TimeStamp', 'Event']
+csv_fpath = ""
+log_fpath = ""
+csv_fname = "_log_100_server_events.csv"
+log_fname = "_video_capture_server_debug.log"
+vid_fdir  = ""
+vid_dir   = "_rec_vids/"
+vid_fname = "_rec_vids.mp4"
+
+
+# source = cv2.imread("fiducial_test.png", cv2.IMREAD_GRAYSCALE)
+template_a = cv2.imread("fiducial_template_a.png", cv2.IMREAD_GRAYSCALE)
+
+
+def match_markers(source, template_a):
+    # Perform scale-invariant template matching
+    # best_match = None
+    # best_val = -np.inf
+    # Store the top-k matches
+
+    matches = []
+
+    top_k = 5
+    scales = [0.5, 0.75, 1.0, 1.25, 1.5]  # Define scales to resize the template
+    # angles = [0, 11.25, 22.5, 33.75, 45, 56.25, 67.5, 78.75, 90]  # Define angles to rotate the template
+    angles = [0, 22.5, 45, 67.5, 90]  # Define angles to rotate the template
+
+    template_to_match = template_a
+
+    for angle in angles:
+        # Rotate the template
+        center = (template_to_match.shape[1] // 2, template_to_match.shape[0] // 2)
+        rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+        rotated_template = cv2.warpAffine(template_to_match, rotation_matrix, (template_to_match.shape[1], template_to_match.shape[0]))
+
+        for scale in scales:
+            # Resize the rotated template
+            resized_template = cv2.resize(rotated_template, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
+            result = cv2.matchTemplate(source, resized_template, cv2.TM_CCOEFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+            # Store the match details
+            matches.append((max_val, max_loc, resized_template.shape[::-1]))
+
+    # Remove duplicate matches based on location
+    unique_matches = []
+    seen_locations = set()
+
+    for match in matches:
+        _, location, _ = match
+        if location not in seen_locations:
+            unique_matches.append(match)
+            seen_locations.add(location)
+
+    matches = unique_matches
+
+    # Sort matches by their score in descending order and keep the top-k
+    matches = sorted(matches, key=lambda x: x[0], reverse=True)[:top_k]
+
+    return matches
+
+
+class ClientHandlingWorker(QObject):
+    """Continuously Listen to & Receive from Client
+    parse msg, ignores timeout"""
+    finished = pyqtSignal()
+    stampUpdated = pyqtSignal(object)
+    recStatusUpdated = pyqtSignal(object)
+    fileUpdated = pyqtSignal(object)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.is_capturing = True
+        self.running = True
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Reuse IP:PORT if available
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((HOST, PORT))
+        self.server_socket.listen(1)
+        self.server_socket.settimeout(20)
+        self.client_socket = None
+
+    def run(self):
+        while self.running:
+            try:
+                if self.client_socket:
+                    # Receive data from the client
+                    data = self.client_socket.recv(1024).decode('utf-8')
+                    stamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+                    global FILE_SET
+                    if data[:8] == 'PIKSTART':
+                        logger.debug("START Pick at %r", data[8:])
+                        self.stampUpdated.emit(["ON", data[8:]])
+                        if FILE_SET:
+                            with open(csv_fpath,'a',newline='', encoding='utf8') as fhdl:
+                                f_csv = csv.writer(fhdl)
+                                f_csv.writerow([data[8:],"PICK_STARTED"])
+                        else:
+                            logger.debug("Need to Create File Objects before Logging !!")
+
+                    elif data[:7] == 'PIKSTOP':
+                        logger.debug("STOP Pick at %r", data[7:])
+                        self.stampUpdated.emit(["OFF", data[7:]])
+                        if FILE_SET:
+                            with open(csv_fpath,'a',newline='', encoding='utf8') as fhdl:
+                                f_csv = csv.writer(fhdl)
+                                f_csv.writerow([data[7:],"PICK_FINISHED"])
+                        else:
+                            logger.debug("Need to Create File Objects before Logging !!")
+
+                    elif data == 'RECSTART':
+                        if FILE_SET:
+                            self.recStatusUpdated.emit(["ON", stamp])
+                        else:
+                            logger.debug("Need to Create File Objects before Recording !!")
+
+                    elif data == 'RECSTOP':
+                        if FILE_SET:
+                            self.recStatusUpdated.emit(["OFF", stamp])
+                        else:
+                            logger.debug("Recording Not Started. Need to Create File Objects")
+
+                    elif data[:8] == 'FNAME$$$':
+                        self.fileUpdated.emit("")
+                        logger.debug("Creating File Objects...")
+                        FILE_SET = False
+                        create_file_obj(data[8:], stamp)
+                        start_logging()
+                        FILE_SET = True
+                        logger.debug("GET FNAME %r at %r", data[8:], stamp)
+                        logger.debug("File Objects Created.")
+                        self.fileUpdated.emit(data[8:])
+
+                    elif data[:8] == 'PIKID$$$':
+                        picking_id = data[8:]
+                        logger.debug("NEW PICKID %r at %r", picking_id, stamp)
+                        if FILE_SET:
+                            with open(csv_fpath,'a',newline='', encoding='utf8') as fhdl:
+                                f_csv = csv.writer(fhdl)
+                                f_csv.writerow([stamp,f"NEWPICKID:{picking_id}"])
+                        else:
+                            logger.debug("Need to Create File Objects before Logging !!")
+                    elif data == '':
+                        pass
+                    else:
+                        logger.debug("Got DATA %r", data)
+                else:
+                    logger.debug("Server is listening on %s:%s", HOST, PORT)
+                    self.client_socket, addr = self.server_socket.accept()
+                    logger.debug("Connection from %s:%s", addr[0], addr[1])
+            except socket.timeout:
+                if self.running:
+                    pass
+                else:
+                    break
+
+    def close_all_conn(self):
+        """Close all Connections"""
+        self.running = False
+        try:
+            self.server_socket.shutdown(socket.SHUT_RDWR)
+            self.server_socket.close()
+        except Exception:
+            pass
+        self.server_socket = None
+        self.client_socket = None
+        self.finished.emit()
+
+class VideoCaptureWorker(QObject):
+    """Capture and Save opencv cam port to the required Spec"""
+    finished = pyqtSignal()
+    frameUpdated = pyqtSignal(object)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.is_capturing = False
+        self.running = True
+        self.timestamp = ""
+        self.filename = ""
+        self.capture = cv2.VideoCapture(CAMERA_NUM, cv2.CAP_V4L2) # Direct Show for MS Windows
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        self.capture.set(cv2.CAP_PROP_EXPOSURE, -6)
+        self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)
+        self.capture.set(cv2.CAP_PROP_FPS, 30)
+        _, frame = self.capture.read()
+        fshape = frame.shape
+        self.fheight = fshape[0]
+        self.fwidth = fshape[1]
+        self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        self.writer = None
+
+    def run(self):
+        while self.running:
+            _, frame = self.capture.read()
+            ret = match_markers(frame, template_a)
+            # print(ret)
+            self.rec_dir_label.setText(str(ret))
+
+            if self.is_capturing:
+                if FILE_SET:
+                    self.writer.write(frame)
+            self.frameUpdated.emit(frame)
+
+    def stop_capturing(self):
+        self.is_capturing = False
+        if FILE_SET:
+            logger.debug("Recording: STOP")
+            self.finished.emit()
+        else:
+            logger.debug("Recording not started")
+        time.sleep(0.1)
+        if self.writer:
+            self.writer.release()
+
+    def start_capturing(self):
+        if FILE_SET:
+            logger.debug("Recording: START")
+            self.timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            logger.debug("Current Timestamp: %s", self.timestamp)
+            self.filename = vid_fdir + self.timestamp + vid_fname
+            self.writer = cv2.VideoWriter(
+                self.filename, self.fourcc, 30.0, (self.fwidth, self.fheight)
+            )
+            self.is_capturing = True
+        else:
+            logger.debug("Need to Create File Objects before record START !!")
+
+    def update_once(self):
+        _, frame = self.capture.read()
+        self.frameUpdated.emit(frame)
+
+class WebcamCaptureApp(QWidget):
+    """GUI APP"""
+    def __init__(self):
+        super().__init__()
+        self.base_time = time.time()
+        self.rec_start_time = None
+        self.video_timer = QTimer()
+        self.video_timer.setInterval(1000)
+
+        logch = logging.StreamHandler()
+        logch.setLevel(logging.DEBUG)
+        logch.setFormatter(formatter)
+        logger.addHandler(logch)
+
+        self.init_ui()
+
+        try:
+            self.video_worker = VideoCaptureWorker()
+            self.start_rec.clicked.connect(self.video_worker.start_capturing)
+            self.stop_rec.clicked.connect(self.video_worker.stop_capturing)
+            self.start_rec.clicked.connect(self.update_ui_rec_on)
+            self.stop_rec.clicked.connect(self.update_ui_rec_off)
+            self.video_timer.timeout.connect(self.update_timer_label)
+            self.video_worker.frameUpdated.connect(self.update_video)
+            self.video_thread = QThread()
+            self.video_worker.moveToThread(self.video_thread)
+            self.video_thread.started.connect(self.video_worker.run)
+            self.video_thread.start()
+
+            self.client_handler = ClientHandlingWorker()
+            self.client_thread = QThread()
+            self.client_handler.moveToThread(self.client_thread)
+            self.client_thread.started.connect(self.client_handler.run)
+            self.client_handler.stampUpdated.connect(self.rec_timestamp)
+            self.client_handler.recStatusUpdated.connect(self.rec_video_updated)
+            self.client_handler.fileUpdated.connect(self.rec_dir_updated)
+            self.client_thread.start()
+
+        except KeyboardInterrupt:
+            logger.debug("Keyboard interrupt received. Shutting down webcam server...")
+            sys.exit(0)
+
+    def init_ui(self):
+        """Set Up GUI"""
+        self.setWindowTitle("Webcam Capture Viewer")
+        self.video_label = QLabel(self)
+        self.rec_status_label = QLabel("Recording OFF")
+        self.rec_status_label.setStyleSheet("color: rgb(0, 0, 0); font-size: 12pt")
+        self.timer_label = QLabel("0:00:00")
+        self.rec_timestamp_label = QLabel(" ")
+        self.rec_timestamp_label.setStyleSheet("font-size: 12pt")
+        self.rec_dir_label = QLabel(" ")
+        self.start_rec = QPushButton("Start Recording")
+        self.stop_rec = QPushButton("Stop Recording")
+        self.start_rec.setEnabled(True)
+        self.stop_rec.setEnabled(False)
+
+        layout_main = QVBoxLayout()
+        layout_rec = QHBoxLayout()
+        layout_time = QHBoxLayout()
+        layout_button = QHBoxLayout()
+
+        layout_rec.addWidget(self.rec_status_label)
+        layout_rec.addWidget(self.rec_timestamp_label)
+        layout_button.addWidget(self.start_rec)
+        layout_button.addWidget(self.stop_rec)
+        layout_time.addWidget(self.timer_label)
+        layout_time.addWidget(self.rec_dir_label)
+
+        layout_main.addWidget(self.video_label)
+        layout_main.addLayout(layout_rec)
+        layout_main.addLayout(layout_time)
+        layout_main.addLayout(layout_button)
+
+        self.setLayout(layout_main)
+        self.show()
+
+    def update_timer_label(self):
+        time_str = f"{datetime.timedelta(seconds = int(time.time() - self.rec_start_time))}"
+        self.timer_label.setText(time_str)
+
+    def rec_timestamp(self, recv):
+        status_str = "Pick Started: " if recv[0] == "ON" else "Pick Finished: "
+        self.rec_timestamp_label.setText(status_str + recv[1])
+
+    def rec_video_updated(self, recv):
+        if recv[0] == "ON":
+            if not self.video_worker.is_capturing:
+                self.start_rec.click()
+        elif recv[0] == "OFF":
+            if self.video_worker.is_capturing:
+                self.stop_rec.click()
+
+    def rec_dir_updated(self, recv):
+        if str(recv) == "":
+            self.rec_dir_label.setText("Rec Dir updating...")
+        else:
+            self.rec_dir_label.setText("Current Rec Dir: "+str(recv))
+
+    def update_ui_rec_on(self):
+        if FILE_SET:
+            self.rec_status_label.setText("Recording ON")
+            self.rec_status_label.setStyleSheet("color: rgb(255, 0, 0); font-size: 12pt")
+            self.rec_start_time = time.time()
+            self.video_timer.start()
+            self.stop_rec.setEnabled(True)
+            self.start_rec.setEnabled(False)
+            with open(csv_fpath,'a',newline='', encoding='utf8') as fhdl:
+                f_csv = csv.writer(fhdl)
+                f_csv.writerow(
+                    [datetime.datetime.now().strftime("%Y%m%d_%H%M%S"),"REC_STARTED"]
+                )
+
+    def update_ui_rec_off(self):
+        if FILE_SET:
+            self.rec_status_label.setText("Recording OFF")
+            self.rec_status_label.setStyleSheet("color: rgb(0, 0, 0); font-size: 12pt")
+            self.video_timer.stop()
+            self.rec_start_time = None
+            logger.debug("Recording time elapsed: %s", self.timer_label.text())
+            self.timer_label.setText("0:00:00")
+            self.start_rec.setEnabled(True)
+            self.stop_rec.setEnabled(False)
+            with open(csv_fpath,'a',newline='', encoding='utf8') as fhdl:
+                f_csv = csv.writer(fhdl)
+                f_csv.writerow(
+                    [datetime.datetime.now().strftime("%Y%m%d_%H%M%S"),"REC_STOPPED"]
+                )
+
+    def update_video(self, frame):
+        height, width, _ = frame.shape
+        bytes_per_line = 3 * width
+        q_image = QImage(frame.data, width, height,
+                         bytes_per_line, QImage.Format_RGB888).rgbSwapped()
+        pixmap = QPixmap.fromImage(q_image)
+        pixmap_scaled = pixmap.scaled(QSize(960,540), Qt.KeepAspectRatio)
+        self.video_label.setPixmap(pixmap_scaled)
+
+    def closeEvent(self, _):
+        """Program Exit handling"""
+        self.client_handler.close_all_conn()
+        self.video_worker.stop_capturing()
+        self.video_worker.running = False
+        # self.video_thread.wait()
+        # self.client_thread.wait()
+        self.video_thread.quit()
+        self.client_thread.quit()
+        # self.video_thread.deleteLater()
+        # self.client_handler.deleteLater()
+        logger.debug("Shutting down webcam server...")
+
+def start_logging():
+    logfh = logging.FileHandler(log_fpath)
+    logfh.setLevel(logging.DEBUG)
+    logfh.setFormatter(formatter)
+    logger.addHandler(logfh)
+
+    with open(csv_fpath,'a',newline='', encoding='utf8') as fhdl:
+        f_csv = csv.writer(fhdl)
+        f_csv.writerow(csv_fields)
+
+def create_file_obj(base_path, init_time):
+    """Update global fnames and fpaths"""
+    global csv_fpath, log_fpath, vid_fdir
+    csv_fpath = base_path + "_csv_files/" + init_time + csv_fname
+    log_fpath = base_path + log_fname
+    vid_fdir = base_path + vid_dir
+    if not os.path.isdir(vid_fdir):
+        os.mkdir(vid_fdir)
+    if not os.path.isdir(base_path + "_csv_files/"):
+        os.mkdir(base_path + "_csv_files/")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='webcam_server',
+        description='A Webcam server listening on 127.0.0.1, port 12345',
+        epilog='Text at the bottom of help'
+    )
+    parser.add_argument('-m', '--mode', action='store', nargs=1)  # optional flag
+    parser.add_argument('-p', '--path', action='store', nargs=1)  # optional flag
+    parser.add_argument('-i', '--init_time', action='store', nargs=1)  # optional flag
+    args = parser.parse_args()
+    mode = args.mode[0]
+    if mode == "v":
+        FILE_SET = True
+        create_file_obj(args.path[0], args.init_time[0])
+        start_logging()
+    app = QApplication(sys.argv)
+    window = WebcamCaptureApp()
+    sys.exit(app.exec_())
